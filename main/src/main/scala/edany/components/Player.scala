8c694b7ef2d209b53d29b7192c42dc2abc3b5491
Kai Sellgren <kaisellgren@gmail.com>
2014-04-02 18:55:58 +0300

    Added movement

--- main/src/main/scala/edany/components/Player.scala
+++ main/src/main/scala/edany/components/Player.scala
@@ -1,10 +1,19 @@
 package edany.components
 
-import edany.Entity
+import edany.{Game, Entity}
 import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input.Keys
 
 case class Player(
   rectangle: Rectangle,
   texture: Texture
-) extends Entity
+) extends Entity {
+  override def update(game: Game) {
+    if (Gdx.input.isKeyPressed(Keys.LEFT)) rectangle.x -= 200 * Gdx.graphics.getDeltaTime
+    if (Gdx.input.isKeyPressed(Keys.RIGHT)) rectangle.x += 200 * Gdx.graphics.getDeltaTime
+    if (Gdx.input.isKeyPressed(Keys.UP)) rectangle.y += 200 * Gdx.graphics.getDeltaTime
+    if (Gdx.input.isKeyPressed(Keys.DOWN)) rectangle.y += 200 * Gdx.graphics.getDeltaTime
+  }
+}
